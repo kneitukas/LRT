2ba265d3c38795ce26ab1643354870ba6115948c
Tomas Kneitas <nosheep6@gmail.com>
2019-04-25 01:16:30 +0300

    final

--- LRT-SPA/src/app/app-routing.module.ts
+++ LRT-SPA/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChannelListComponent } from './components/channel-list/channel-list.component';
 import { DataResolver } from './resolvers/data.resolver'
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', component: ChannelListComponent, resolve: {channels: DataResolver} }
+  {path: '', component: ChannelListComponent, resolve: {channels: DataResolver} },
+  {path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- LRT-SPA/src/app/app.module.ts
+++ LRT-SPA/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChannelListComponent } from './components/channel-list/channel-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChannelListComponent
+    ChannelListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
